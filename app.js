7e5981cf321c8c58431de8d1f4e847f59f510939
Elisa Chiappini <elisabichiappini@gmail.com>
2025-01-11 12:35:03 +0100

    feat: assegnare la api key

--- app.js
+++ app.js
@@ -3,11 +3,11 @@
 //     console.log(element)
 // });
 
-const prodotti = process.argv.slice(2);
-console.log('La mia lista della spesa:\n')
-prodotti.forEach((element, index) => {
-    console.log(`${index+1} - ${element}`);
-});
+// const prodotti = process.argv.slice(2);
+// console.log('La mia lista della spesa:\n')
+// prodotti.forEach((element, index) => {
+//     console.log(`${index+1} - ${element}`);
+// });
 
 /*node app.js frutta verdura carote
 La mia lista della spesa:
@@ -16,3 +16,11 @@ La mia lista della spesa:
 3 - carote
 elisachiappini@MacBook-Pro-di-Elisa-3 react-1 % 
 */
+
+
+const apiKey = process.env.apiKey;
+console.log(`La tua api key è: ${apiKey}`);
+
+/*
+apiKey="ahskd3lsxdk" node app.js
+La tua api key è: ahskd3lsxdk*/
