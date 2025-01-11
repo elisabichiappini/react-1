b736a516decdb56aba1c02e93e1f35fa90a3cdd2
Elisa Chiappini <elisabichiappini@gmail.com>
2025-01-11 13:07:13 +0100

    feat: separazione lista a ogni virgola

--- app.js
+++ app.js
@@ -1,26 +1,10 @@
-// const argomenti = process.argv.slice(2);
-// argomenti.forEach(element => {
-//     console.log(element)
-// });
-
-// const prodotti = process.argv.slice(2);
-// console.log('La mia lista della spesa:\n')
-// prodotti.forEach((element, index) => {
-//     console.log(`${index+1} - ${element}`);
-// });
-
-/*node app.js frutta verdura carote
-La mia lista della spesa:
-1 - frutta
-2 - verdura
-3 - carote
-elisachiappini@MacBook-Pro-di-Elisa-3 react-1 % 
-*/
-
-
-const apiKey = process.env.apiKey;
-console.log(`La tua api key è: ${apiKey}`);
+const prodotti = process.env.LISTA.split(','); 
+prodotti.forEach((p, index) => {
+    console.log(`${index + 1}. ${p}`);
+});
 
 /*
-apiKey="ahskd3lsxdk" node app.js
-La tua api key è: ahskd3lsxdk*/
+elisachiappini@MacBook-Pro-di-Elisa-3 react-1 % LISTA="carote,pere,salame" node app.js 
+1. carote
+2. pere
+*/
