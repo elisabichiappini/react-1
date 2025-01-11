e36c5dcdd07f43e86d4f178d6834b6b1df2a0ace
Elisa Chiappini <elisabichiappini@gmail.com>
2025-01-11 12:24:43 +0100

    feat: lista spesa

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
-const argomenti = process.argv.slice(2);
-argomenti.forEach(element => {
-    console.log(element)
+// const argomenti = process.argv.slice(2);
+// argomenti.forEach(element => {
+//     console.log(element)
+// });
+
+const prodotti = process.argv.slice(2);
+console.log('La mia lista della spesa:')
+prodotti.forEach((element, index) => {
+    console.log(`${index+1} - ${element}`);
 });
+
+/*node app.js frutta verdura carote
+La mia lista della spesa:
+1 - frutta
+2 - verdura
+3 - carote
+elisachiappini@MacBook-Pro-di-Elisa-3 react-1 % 
+*/
